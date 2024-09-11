b83db3f411d9370580580c64a2d89f2bfe272680
Pedro Martins <62296977+PedrMartins@users.noreply.github.com>
2024-09-11 16:46:44 -0300

    Add files via upload

---
+++ email_test_script.R
@@ -0,0 +1,19 @@
+library(blastula)
+library(rmarkdown)
+
+source("import_and_processing_data.R")
+
+
+
+
+if (!colSums(ultima_medicao) == 0) {
+
+  my_email_object = render_email("email_test_obje.Rmd")
+  smtp_send(my_email_object,
+          from = "ggnot_throwaway@outlook.com",
+          to = "ggnot_throwaway@outlook.com",
+          subject = paste0("Erro sensores ", Sys.Date()),
+          credentials = creds_file("ggnot_throwaway_creds"))
+} else {
+  cat ("nenhum problema a relatar")
+  }
