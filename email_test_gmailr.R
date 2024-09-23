0e6c01a22ec75e3eea09a0f8436e736c0470d496
Pedro Martins <62296977+PedrMartins@users.noreply.github.com>
2024-09-23 13:43:32 -0300

    Create email_test_gmailr.R

---
+++ email_test_gmailr.R
@@ -0,0 +1,20 @@
+library(blastula)
+library(rmarkdown)
+
+source("import_and_processing_data.R")
+
+# Fetch the email credentials path from environment variable
+email_creds <- Sys.getenv("EMAIL_CREDENTIALS_PATH")
+
+my_email_object = render_email("email_test_obje.Rmd")
+
+if (!colSums(ultima_medicao) == 0) {
+
+ 
+  smtp_send(my_email_object,
+          from = "iotree-email-test@just-genius-337505.iam.gserviceaccount.com",
+          to = "pedro.rufino.martins@gmail.com",
+          subject = paste0("Erro sensores ", Sys.Date()))
+} else {
+  print ("nenhum problema à relatar")
+  }
