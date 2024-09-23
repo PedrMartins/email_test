9dd2c9200b80ddfa39fd31d8fad0c1773f64f822
Pedro Martins <62296977+PedrMartins@users.noreply.github.com>
2024-09-23 13:44:33 -0300

    Update email_test_gmailr.R

--- email_test_gmailr.R
+++ email_test_gmailr.R
@@ -1,20 +1,22 @@
-library(blastula)
+library(gmailr)
 library(rmarkdown)
 
 source("import_and_processing_data.R")
 
-# Fetch the email credentials path from environment variable
-email_creds <- Sys.getenv("EMAIL_CREDENTIALS_PATH")
+# Configuração da autenticação com Gmail API
+gm_auth_configure(path = Sys.getenv("EMAIL_CREDENTIALS_PATH"))
+gm_auth(email = TRUE)
 
-my_email_object = render_email("email_test_obje.Rmd")
+# Gerar o corpo do email a partir do R Markdown
+my_email_object <- render_email("email_test_obje.Rmd")
 
 if (!colSums(ultima_medicao) == 0) {
-
- 
-  smtp_send(my_email_object,
-          from = "iotree-email-test@just-genius-337505.iam.gserviceaccount.com",
-          to = "pedro.rufino.martins@gmail.com",
-          subject = paste0("Erro sensores ", Sys.Date()))
-} else {
-  print ("nenhum problema à relatar")
-  }
+  # Criar o email com o pacote gmailr
+  email <- gm_mime() %>%
+    gm_to("pedro.rufino.martins@gmail.com") %>%
+    gm_from("iotree-email-test@just-genius-337505.iam.gserviceaccount.com") %>%
+    gm_subject(paste0("Erro sensores ", Sys.Date())) %>%
+    gm_html_body(my_email_object)
+  
+  # Enviar o email usando a API do Gmail
+  gm_send_message(email)
