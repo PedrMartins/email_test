6e114fa30703dcb11c3399a34a7ac26a081ffd57
Pedro Martins <62296977+PedrMartins@users.noreply.github.com>
2024-09-24 11:49:23 -0300

    Update email_test_script.R

--- email_test_script.R
+++ email_test_script.R
@@ -4,17 +4,25 @@ library(rmarkdown)
 source("import_and_processing_data.R")
 
 # Fetch the email credentials path from environment variable
-email_creds <- Sys.getenv("EMAIL_CREDENTIALS_PATH")
 
-my_email_object = render_email("email_test_obje.Rmd")
+
+
 
 if (!colSums(ultima_medicao) == 0) {
 
+ email_creds <- Sys.getenv("EMAIL_CREDENTIALS_PATH")
+ 
+ email_creds <- create_smtp_creds_file (email_creds)
+ 
+  my_email_object = render_email("email_test_obje.Rmd")
  
   smtp_send(my_email_object,
           from = "iotree-email-test@just-genius-337505.iam.gserviceaccount.com",
           to = "pedro.rufino.martins@gmail.com",
-          subject = paste0("Erro sensores ", Sys.Date()))
+          subject = paste0("Erro sensores ", Sys.Date()),
+          credentials = creds_file (email_creds)
+           )
+ 
 } else {
   print ("nenhum problema à relatar")
   }
