06e29cc354e8bdd59e9069eb9c8226f938f9fc75
Pedro Martins <62296977+PedrMartins@users.noreply.github.com>
2024-09-26 16:21:06 -0300

    Update email_test_script.R

--- email_test_script.R
+++ email_test_script.R
@@ -1,11 +1,16 @@
 library(blastula)
 library(rmarkdown)
 
+smtp_user <- Sys.getenv("SMTP_USER")
+smtp_pass <- Sys.getenv("SMTP_PASS")
 
-create_smtp_creds_file("iot_creds",
-                       user="gthub_iot@outlook.com",
-                       provider = "outlook")
-
+# Create SMTP credentials file using environment variables
+create_smtp_creds_file(
+  file = "iot_creds",
+  user = smtp_user,
+  provider = "outlook",
+  password = smtp_pass
+)
 
 source("import_and_processing_data.R")
 
