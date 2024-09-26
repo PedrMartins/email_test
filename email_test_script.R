c155f468ca37f8d646d4390a56920458fa7617d5
Pedro Martins <62296977+PedrMartins@users.noreply.github.com>
2024-09-26 16:35:57 -0300

    Update email_test_script.R

--- email_test_script.R
+++ email_test_script.R
@@ -1,16 +1,7 @@
 library(blastula)
 library(rmarkdown)
 
-smtp_user <- Sys.getenv("SMTP_USER")
-smtp_pass <- Sys.getenv("SMTP_PASS")
-
-# Create SMTP credentials file using environment variables
-create_smtp_creds_file(
-  file = "iot_creds",
-  user = smtp_user,
-  provider = "outlook",
-  password = smtp_pass
-)
+smtp_creds <- Sys.getenv("SMTP_CREDS")
 
 source("import_and_processing_data.R")
 
@@ -25,7 +16,7 @@ if (!colSums(ultima_medicao) == 0) {
             from = "gthub_iot@outlook.com",
             to = "pedro.rufino.martins@gmail.com",
             subject = paste0("Erro sensores ", Sys.Date()),
-            credentials = creds_file("iot_creds"))
+            credentials = creds_file(smtp_creds))
 } else {
   cat ("nenhum problema à relatar")
 }
