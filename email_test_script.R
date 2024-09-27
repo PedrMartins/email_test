b9f0fe9ff8db3fab8ca50b7fe146f2c0dbac7f89
Pedro Martins <62296977+PedrMartins@users.noreply.github.com>
2024-09-27 10:20:57 -0300

    Update email_test_script.R

--- email_test_script.R
+++ email_test_script.R
@@ -1,9 +1,6 @@
 library(blastula)
 library(rmarkdown)
 
-smtp_creds <- Sys.getenv("SMTP_PASS")
-
-
 source("import_and_processing_data.R")
 
 if (!colSums(ultima_medicao) == 0) {
@@ -11,20 +8,15 @@ if (!colSums(ultima_medicao) == 0) {
   
   objct <- render_email("email_test_obje.Rmd")
 
-  getwd ()
-
+  
   smtp_send(objct,
             from = "gthub_iot@outlook.com",
             to = "pedro.rufino.martins@gmail.com",
             subject = paste0("Erro sensores ", Sys.Date()),
-            credentials = creds_envvar(
-              user="gthub_iot@outlook.com"
-              provider=
-              pass_envvar = Sys.setenv(smtp_creds)
-            )
+            credentials = creds_file ("iot_creds")
            )
 } else {
-  cat ("nenhum problema à relatar")
+  print ("nenhum problema à relatar")
 }
 
 
